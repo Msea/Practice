6b34727df26634cfe92100298f09b817207013a6
Msea <Msea0j@gmail.com>
2013-12-31 02:59:32 -0500

    Update bs.py

--- bs.py
+++ bs.py
@@ -1,5 +1,4 @@
 from random import randint
-#boardsize=9
 
 def print_board(board):
     for row in board:
@@ -22,14 +21,12 @@ def hide(ship, board, leng, i):
         v=False
         while v==False:
             ornt = raw_input("Do you wish your " + ship + " to be hidden vertically (enter v) or horizontally (enter h) ")
-            #location = [col, row, ornt]
             if ((ornt == "v") or (ornt =="h")):
                 v=True
             else:
                 print "Invalid Position"
         v=False
         while v==False:
-            #col = int(raw_input("In what column (0-"+str(boardsize-1)+") do you wish to begin hiding your " + ship + "? " ))
             col=(raw_input("In what column (0-"+str(boardsize-1)+") do you wish to begin hiding your " + ship + "? " ))
             try:
                col = int(col)
@@ -68,32 +65,24 @@ def hide(ship, board, leng, i):
                         v=True
                     else:
                         print "There is no water there. Ship cannot stay hidden well if it is on dry land."
-        #print valid    
         if ornt == "v":
             for j in range(row, row+leng):
                 if (not (board[j][col] == 'O')):
-                    #board[j][col] = i
                     valid = -1
         if ornt == "h":
             for j in range(col, col+leng):
                 if (not (board[row][j]=='O')):
-                    #board[row][j] = i
-                    #print "this this"
-                    #print valid
                     valid = -1
         if (valid == 0):
             if ornt == "v":
                 for j in range(row, row+leng):
-                    #if (board[j][col] == 'O'):
                     board[j][col] = i
             else:
                 for j in range(col, col+leng):
-                    #if (board[row][j]=='O'):
                     board[row][j] = i
                     
             valid = 2
         else:
-            #valid=0
             print "You have already hidden something there. Hide elsewhere"
    
     print "Your "+ship+ " has been hidden."
@@ -110,86 +99,51 @@ def hidden(ship, board, leng, i):
         valids = 0
         valid = False
         location = [randint(0, boardsize-1), randint(0, boardsize-1), randint(0, 1)]
-        #print location
         startingrow = location[0]
         startingcol=location[1]
         if location[2] == 1:
             ornt = "v"
-            #print "this is executing"
             if (startingrow+leng<=boardsize):
                 valid =True
-                #print i + " vertical fits on board"
         else:
             ornt = "h"
-            #print "this is executing this time"
             if (startingcol+leng<=boardsize):
                 valid =True
-                #print i+"horizontal fits on board"
         if valid ==True:
-            #print location
             if ornt == "h":
                 for j in range(startingcol, startingcol+leng):
-                    #print j
                     if(not(board[startingrow][j] == 'O')):
                         valids =-1
-                        #print board[j][startingcol]
-                        #print i+"overlapps as many times as this"
-                        #apple = raw_input("In")
-                    #else:
-                        #print board[j][location[1]] +"is O"
             else:
                 for j in range(startingrow, startingrow+leng):
-                    #print j
                     if(not(board[j][startingcol]=='O')):
                         valids =-1
-                        #print board[location[0]][j]
-                        #print j
-                        #print location[0]
-                        #print i+"overlapps as many times as this"
-                        #apple = raw_input("In")
-                    #else:
-                        #print board[location[0]][j] +"is O"
             if valids == 0:
                 valids = 1
-                #print "doesn't overlap"
     if ornt == "v":
         for j in range(startingrow, startingrow+leng):
-            #print board[j][location[1]] +"is O"
             board[j][startingcol] = i
-            #print board
-            #print ornt
     else:
         for j in range(startingcol, startingcol+leng):
-            #print board[location[0]][j] +"is O"
             board[startingrow][j]=i
-            #print board
-            #print ornt
     apple = raw_input("Press Enter for Computer to hide "+ship)
     print "An intelligence report has come in that computer has hidden "+ship+" Why did you press Enter??"
     return board
 
 def win(board):
-    #print "this is working"
     winned = True
     for row in board:
-        #print "still working (shuld prnt 10 times)"
         for point in row:
             if (point == "a"):
                 winned = False
-                #print "should print 5/4 times"
             elif (point =='b'):
                 winned = False
-                #print "no probs here"
             elif (point =='c'):
                 winned = False
-                #print "this is tedious"
             elif (point =='d'):
                 winned = False
-                #print "stuffs"
             elif (point =='s'):
                 winned = False
-                #print "should print"
-    #print str(winned) + "T/F value that should be returnable / calculatable"
     return winned
 
 def sunk(board, i):
@@ -201,7 +155,6 @@ def sunk(board, i):
     return sunked
 
 def guess(board, guess):
-    #winned = False
     new = False
     if (board[guess[0]][guess[1]] == 'O'):
         new = True
@@ -213,48 +166,35 @@ def guess(board, guess):
         print "Aircraft Carrier has been hit!"
         if sunk(board, 'a'):
             print "Aircraft Carrier has been sunk"
-            #if win(board):
-                #winned = True
     elif (board[guess[0]][guess[1]] == 'b'):
         new = True
         board[guess[0]][guess[1]] = 'B'
         print "Battleship has been hit!"
         if sunk(board, 'b'):
             print "Battleship has been sunk"
-            #if win(board):
-                #winned = True
     elif (board[guess[0]][guess[1]] == 'c'):
         new = True
         board[guess[0]][guess[1]] = 'C'
         print "Cruiser has been hit!"
         if sunk(board, 'c'):
             print "Cruiser has been sunk"
-            #if win(board):
-                #winned = True
     elif (board[guess[0]][guess[1]] == 'd'):
         new = True
         board[guess[0]][guess[1]] = 'D'
         print "Destroyer has been hit!"
         if (sunk(board, 'd')):
             print "Destroyer has been sunk"
-            #if (win(board)):
-                #winned = True
     elif (board[guess[0]][guess[1]] == 's'):
         new = True
         board[guess[0]][guess[1]] = 'S'
         print "Submarine has been hit!"
         if sunk(board, 's'):
             print "Submarine has been sunk"
-            #if (win(board)):
-                #winned = True
-    #return(board, winned, new)
-    #print_board(board)
     return(board, new)
 
 def humanguess(board):
     validguess = False
     validsguess = False
-    #humwin = False
     while (validsguess ==False):
         validguess = False
         print "Here is what the enemy ocean looks like thus far:"
@@ -269,7 +209,6 @@ def humanguess(board):
            print "Low level missiles cannot reach that hyperplane."
         if temp2:
            launch.append(temp)
-        #print "To what coordinates (0-9) do you wish to launch this missle? Input as (row, column)"
            temp = raw_input("To which column coordinate (0-"+str(boardsize-1)+") do you wish to launch this missile? ")
            try:
                temp = int(temp)
@@ -279,7 +218,6 @@ def humanguess(board):
                print "You hit the treasury for your missile supplier and caused confusion. You get a free additional launch."
            if temp2:
                launch.append(temp)
-        #launch.append(int(raw_input("To which column coordinate (0-"+str(boardsize-1)+") do you wish to launch this missile? ")))
                if (launch[0]<0 or launch[0]>=boardsize or launch[1]<0 or launch[1]>=boardsize):
               
                     print "That is beyond the scope of our missiles. Command a launch to somewhere else."
@@ -294,8 +232,6 @@ def humanguess(board):
             apple = raw_input("Press Enter to continue...")
     board = (guess(board, launch))[0]
     stealth_print(board)
-    #humwin = (guess(board, launch))[1]
-    #return (board, humwin)
     return(board)
 
 def compguess(board): 
@@ -351,7 +287,6 @@ if not abridge:
    board2 = hidden("submarine", board2, 3, "s")
    board2 = hidden("cruiser", board2, 3, "c")
    board2 = hidden("destroyer", board2, 2, "d")
-#print_board(board2)
 
 
 print "The sun sets in the east over a cold sky while a green breeze hovers below the treetops. The winds are changing...war will soon be upon us."
